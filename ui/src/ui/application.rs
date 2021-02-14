7de083a80f855600d3616e87c3db3f9bcc3a3fc3
ferd <beowulf1416@gmail.com>
2021-02-14 23:35:52 +0800

    adding "quit" action and associating to menu. bug - file -> quit menu is always disabled.

--- ui/src/ui/application.rs
+++ ui/src/ui/application.rs
@@ -1,15 +1,21 @@
 use gio::prelude::*;
 use gtk::prelude::*;
-// use gtk_macros::{ get_widget };
+use gtk_macros::{ get_widget, action };
 use glib::clone;
 
 use std::env;
 
 use crate::constants::{ APP_ID, WINDOW_UI, EDITOR_SQL_UI };
 
+
+enum AppEvent {
+    Quit
+}
+
 pub struct Application {
     app: gtk::Application,
-    window: gtk::ApplicationWindow
+    window: gtk::ApplicationWindow,
+    sender: glib::Sender<AppEvent>
 }
 
 impl Application {
@@ -30,14 +36,17 @@ impl Application {
         //     Some("test")
         // );
 
-        // gtk_app.add_window(&window);
+        let (sender, receiver) = glib::MainContext::channel::<AppEvent>(glib::PRIORITY_DEFAULT);
 
         let application = Self {
             app: gtk_app,
-            window: window
+            window: window,
+            sender: sender
         };
 
+        application.setup_actions();
         application.attach_signal_handlers();
+        application.setup_receiver(receiver);
 
         return Ok(application);
     }
@@ -47,12 +56,45 @@ impl Application {
         self.app.run(&args);
     }
 
+    fn setup_actions(&self) {
+        action!(self.app, "app.quit", 
+            clone!(@strong self.sender as sender => move |_, _| {
+                sender.send(AppEvent::Quit).unwrap();
+            })
+        );
+    }
+
+    fn setup_receiver(&self, receiver: glib::Receiver<AppEvent>) {
+        receiver.attach(
+            None,
+            clone!(
+                @strong self.sender as sender, 
+                @strong self.app as app, 
+                @strong self.window as window => move |event| {
+
+                match event {
+                    AppEvent::Quit => {
+                        app.quit();
+                    }
+                }
+
+                return glib::Continue(true);
+            })
+        );
+    }
+
     fn attach_signal_handlers(&self) {
         self.app.connect_activate(
             clone!(@weak self.window as window => move |app| {
                 window.set_application(Some(app));
                 app.add_window(&window);
                 window.present();
+            })
+        );
+
+        self.window.connect_destroy(
+            clone!(@strong self.sender as sender => move |_| {
+                sender.send(AppEvent::Quit).unwrap();
             })
         );
     }
