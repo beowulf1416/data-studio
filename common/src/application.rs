f53b98249963ba49c232d571a8d981a5612b8188
ferd <beowulf1416@gmail.com>
2021-02-23 21:26:45 +0800

    enumerate plugins in directory

--- common/src/application.rs
+++ common/src/application.rs
@@ -1,3 +1,4 @@
+use std::{ env, fs };
 
 use crate::providers::registry::{ Registry };
 
@@ -13,6 +14,20 @@ impl Application {
         let application = Self {
             registry: Registry::new()
         };
+
+        let current_dir = env::current_dir();
+        println!("{:?}", current_dir);
+
+        let current_exe = env::current_exe().unwrap();
+        let mut path = current_exe;
+        path.pop();
+        println!("{:?}", path);
+
+        for entry in fs::read_dir(path).unwrap() {
+            let f = entry.unwrap();
+            let fname = f.file_name();
+            println!("{:?}", fname);
+        }
         
         return application;
     }
