ab710345a7a1afd27f027f546dad19ce2a69b8d6
ferd <beowulf1416@gmail.com>
2021-03-04 20:25:12 +0800

    modify cli project to list providers

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,24 @@
-// extern crate common;
+// extern crate config;
+extern crate common;
+
+// use std::collections::HashMap;
 
 use common::application::Application;
+
 
 fn main() {
     println!("Hello, world!");
 
-    let app = Application::new();
-    // for (key, value) in app.registry.providers {
+    // let mut settings = config::Config::new();
+    // settings
+    //     .merge(config::File::with_name("config")).unwrap()
+    //     .merge(config::Environment::with_prefix("DS")).unwrap();
 
-    // }
+    // println!("{:?}", settings.try_into::<HashMap<String, String>>().unwrap());
+
+    let app = Application::new();
+    let providers = app.providers();
+    for provider in providers {
+        println!("{}", provider);
+    }
 }
