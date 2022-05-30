b23903494b8572b06ae7fa1f871a71e41dc469af
ferd <beowulf1416@gmail.com>
2022-05-30 21:36:02 +0800

    modify main view model

--- data-studio/src/view_models/main_view_model.rs
+++ data-studio/src/view_models/main_view_model.rs
@@ -1,7 +1,21 @@
+use gtk::{
+    Window
+};
+use relm::{ Widget };
+
 use crate::models::application::Application;
 use crate::views::main_window::MainWindow;
 
 pub struct MainViewModel {
     model: Application,
     view: MainWindow
+}
+
+impl Widget for MainViewModel {
+    type Root = Window;
+
+    fn root(&self) -> Self::Root {
+        return self.view.MainWindow.clone();
+    }    
+
 }
