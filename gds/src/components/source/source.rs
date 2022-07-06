10706928169517403e747a325386ac629d511cf8
ferd <beowulf1416@gmail.com>
2022-07-06 10:18:23 +0800

    add template children

--- gds/src/components/source/source.rs
+++ gds/src/components/source/source.rs
@@ -14,8 +14,23 @@ use gtk::{
 #[derive(CompositeTemplate, Default)]
 #[template(resource="/org/tomale/ds/view.source.ui")]
 pub struct SourceView {
-    // #[template_child]
-    // pub tv: TemplateChild<gtk::TreeView>
+    #[template_child]
+    pub source_name: TemplateChild<gtk::Entry>,
+
+    #[template_child]
+    pub source_type: TemplateChild<gtk::ComboBoxText>,
+
+    #[template_child]
+    pub source_server: TemplateChild<gtk::Entry>,
+
+    #[template_child]
+    pub user_name: TemplateChild<gtk::Entry>,
+
+    #[template_child]
+    pub password: TemplateChild<gtk::Entry>,
+
+    #[template_child]
+    pub remember: TemplateChild<gtk::CheckButton>,
 }
 
 
